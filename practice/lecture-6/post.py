0a936e8485dac693451bdf62fe0d60794928ce1c
IRedDragonICY <hendik.suwoto@gmail.com>
2024-05-20 14:13:22 +0700

    upload

--- practice/lecture-6/post.py
+++ practice/lecture-6/post.py
@@ -1,34 +1,24 @@
 from ai_pkg.planning import (HLA, Problem as PlanProblem)
 
 library = {
-    'HLA': ['Go(Home,Airport)',
-            'Go(Home,Airport)',
-            'Drive(Home, AirportParking)',
-            'Shuttle(AirportParking, Airport)',
+    'HLA': ['Go(Home, Airport)',
             'Taxi(Home, Airport)'],
-    'steps':[['Drive(Home, AirportParking)', 'Shuttle(AirportParking, Airport)'],
-             ['Taxi(Home, Airport)'],
-             [],
-             [],
-             []],
-    'precond': [['At(Home) & Have(Car)'],
-                ['At(Home)'],
-                ['At(Home) & Have(Car)'],
-                ['At(AirportParking)'],
+    'steps': [['Taxi(Home, Airport)'],
+              []],
+    'precond': [['At(Home)'],
                 ['At(Home)']],
-    'effect': [['At(Airport) & ~At(Home)'],
-               ['At(Airport) & ~At(Home) & ~Have(Cash)'],
-               ['At(AirportParking) & ~At(Home)'],
-               ['At(Airport) & ~At(AirportParking)'],
-               ['At(Airport) & ~At(Home) & ~Have(Cash)']] }
+    'effect': [['At(Airport) & ~At(Home) & ~Have(Cash)'],
+               ['At(Airport) & ~At(Home) & ~Have(Cash)']]
+}
 
-goto_airport = HLA('Go(Home, Airport)', precond='At(Home)', effect='At(Airport) & ~At(Home)')
+goto_airport = HLA('Go(Home, Airport)', precond='At(Home)', effect='At(Airport) & ~At(Home) & ~Have(Cash)')
 
-problem = PlanProblem('At(Home) & Have(Cash) & Have(Car) ', 'At(Airport) & Have(Cash)',
+problem = PlanProblem('At(Home) & Have(Cash) & Have(Car)', 'At(Airport) & ~At(Home) & ~Have(Cash)',
                       [goto_airport])
 
 solution = PlanProblem.hierarchical_search(problem, library)
-for i in range(0, len(solution)):
+
+for i in range(len(solution)):
     print('precondition: ', solution[i].precond)
-    print('action: ' , solution[i].name, solution[i].args)
+    print('action: ', solution[i].name, solution[i].args)
     print('effect : ', solution[i].effect)
--- practice/lecture-6/prak2.py
+++ practice/lecture-6/prak2.py
@@ -1,24 +1,34 @@
 from ai_pkg.planning import (HLA, Problem as PlanProblem)
 
 library = {
-    'HLA': ['Go(Home, Airport)',
+    'HLA': ['Go(Home,Airport)',
+            'Go(Home,Airport)',
+            'Drive(Home, AirportParking)',
+            'Shuttle(AirportParking, Airport)',
             'Taxi(Home, Airport)'],
-    'steps': [['Taxi(Home, Airport)'],
-              []],
-    'precond': [['At(Home)'],
+    'steps':[['Drive(Home, AirportParking)', 'Shuttle(AirportParking, Airport)'],
+             ['Taxi(Home, Airport)'],
+             [],
+             [],
+             []],
+    'precond': [['At(Home) & Have(Car)'],
+                ['At(Home)'],
+                ['At(Home) & Have(Car)'],
+                ['At(AirportParking)'],
                 ['At(Home)']],
-    'effect': [['At(Airport) & ~At(Home) & ~Have(Cash)'],
-               ['At(Airport) & ~At(Home) & ~Have(Cash)']]
-}
+    'effect': [['At(Airport) & ~At(Home)'],
+               ['At(Airport) & ~At(Home) & ~Have(Cash)'],
+               ['At(AirportParking) & ~At(Home)'],
+               ['At(Airport) & ~At(AirportParking)'],
+               ['At(Airport) & ~At(Home) & ~Have(Cash)']] }
 
-goto_airport = HLA('Go(Home, Airport)', precond='At(Home)', effect='At(Airport) & ~At(Home) & ~Have(Cash)')
+goto_airport = HLA('Go(Home, Airport)', precond='At(Home)', effect='At(Airport) & ~At(Home)')
 
-problem = PlanProblem('At(Home) & Have(Cash) & Have(Car)', 'At(Airport) & ~At(Home) & ~Have(Cash)',
+problem = PlanProblem('At(Home) & Have(Cash) & Have(Car) ', 'At(Airport) & Have(Cash)',
                       [goto_airport])
 
 solution = PlanProblem.hierarchical_search(problem, library)
-
-for i in range(len(solution)):
+for i in range(0, len(solution)):
     print('precondition: ', solution[i].precond)
-    print('action: ', solution[i].name, solution[i].args)
+    print('action: ' , solution[i].name, solution[i].args)
     print('effect : ', solution[i].effect)
